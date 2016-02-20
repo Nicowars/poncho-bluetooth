9fa9ac7b7040349473408e8b42816d540d1fc3b2
Jameru <jaime.zb0@gmail.com>
2016-02-20 19:57:41 -0300

    Using HEADER-MK2. (hopefully will work)

--- projects/bluetooth-rn4020/src/rn4020.c
+++ projects/bluetooth-rn4020/src/rn4020.c
@@ -15,28 +15,44 @@
 
 static int32_t fd_uart2;
 
-void rn4020_init(){
+void rn4020_Init(){
 	fd_uart2 = ciaaPOSIX_open("/dev/serial/uart/2", ciaaPOSIX_O_RDWR);
 }
 
-void rn4020_echo(){
+void RN4020_SetServices ( int32_t op ){
+    char * str = "SS";
+    char * hex;
+    sprintf(hex, "%x", op);
+    ciaaPOSIX_strcat( str , hex );
+    ciaaPOSIX_write(fd_uart2, str, 12);
+}
+
+void RN4020_SetFeatures ( int32_t op ){
+    char * str = "SR";
+    char * hex;
+    sprintf(hex, "%x", op);
+    ciaaPOSIX_strcat( str , hex );
+    ciaaPOSIX_write(fd_uart2, str, 12);
+}
+
+void rn4020_ToggleEcho(){
 	ciaaPOSIX_write(fd_uart2, "+\n", 2);
 }
 
-void rn4020_factory(){
+void rn4020_PartialFactory(){
 	ciaaPOSIX_write(fd_uart2, "SF,1\n", 5); // Set factory default config.
 
 }
 
-void rn4020_reset(){
+void rn4020_Reboot(){
 	   ciaaPOSIX_write(fd_uart2, "R,1\n", 4); // Reset module
 }
 
-ssize_t rn4020_read(void * buf, size_t nbyte){
-	return ciaaPOSIX_read(fd_uart2, buf, 20);
+ssize_t rn4020_Read(void * buf, size_t nbyte){
+	return ciaaPOSIX_read(fd_uart2, buf, nbyte);
 }
 
-void rn4020_write(void const * buf, size_t nbyte){
+void rn4020_Write(void const * buf, size_t nbyte){
 	 ciaaPOSIX_write(fd_uart2, buf, nbyte);
 }
 
