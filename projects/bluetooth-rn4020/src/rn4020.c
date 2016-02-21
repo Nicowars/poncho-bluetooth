43435aa63cad0025363222c446debc3481fc0273
Nicowars <nicolaslaurella@hotmail.com>
2016-02-20 23:57:34 -0300

    Revert "xto18. Fix1"
    This reverts commit dbc30a1bce1aaf387efd1c8ba1701ec3e7147f4f.

--- projects/bluetooth-rn4020/src/rn4020.c
+++ projects/bluetooth-rn4020/src/rn4020.c
@@ -23,14 +23,9 @@ extern int32_t fd_out;
  * no tiene que estar aca... */
 static char * xtoa8( uint32_t hex ){
     char str[8];
-    char aux;
     int8_t ind;
     for (ind = 0; ind < 8; ind++) {
-        aux = hex % 16; 
-        if (aux < 10)
-            str[7-ind] = aux + '0';
-        else
-            str[7-ind] = aux + 'A' - 10;
+        str[7-ind] = hex % 16;
         hex /= 16;
     }
     return str;
