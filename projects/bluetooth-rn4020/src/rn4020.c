957a1962c80c74f2c578f37da13fc5a940c7073c
Nicowars <nicolaslaurella@hotmail.com>
2016-02-20 23:57:39 -0300

    Revert "xtoa8. Untested."
    This reverts commit 4b30420bb6128e25abb3a8a6a892bd2772da7d83.

--- projects/bluetooth-rn4020/src/rn4020.c
+++ projects/bluetooth-rn4020/src/rn4020.c
@@ -19,19 +19,6 @@ extern int32_t fd_out;
 
 
 
-/* Obviamente esta funcion 
- * no tiene que estar aca... */
-static char * xtoa8( uint32_t hex ){
-    char str[8];
-    int8_t ind;
-    for (ind = 0; ind < 8; ind++) {
-        str[7-ind] = hex % 16;
-        hex /= 16;
-    }
-    return str;
-}
- 
-
 void rn4020_Init(){
 	uint8_t outputs;
 	fd_uart2 = ciaaPOSIX_open("/dev/serial/uart/2", ciaaPOSIX_O_RDWR);
@@ -42,7 +29,7 @@ void rn4020_Init(){
 void RN4020_SetServices ( uint32_t op ){
     char * str = "SS";
     char * hex;
-    hex = xtoa8(op);
+    //sprintf(hex, "%X", op);
     ciaaPOSIX_strcat( str , hex );
     ciaaPOSIX_write(fd_uart2, str, 12);
 }
@@ -50,7 +37,7 @@ void RN4020_SetServices ( uint32_t op ){
 void RN4020_SetFeatures ( uint32_t op ){
     char * str = "SR";
     char * hex;
-    hex = xtoa8(op);
+    //sprintf(hex, "%X", op);
     ciaaPOSIX_strcat( str , hex );
     ciaaPOSIX_write(fd_uart2, str, 12);
 }
